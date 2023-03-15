f29d2a56b0eb110b9e27635bda71053a8a1918d2
tifuseini <tifuseini@st.ug.edu.gh>
2023-03-15 17:41:10 +0000

    data

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,16 @@
+import { IsString } from 'class-validator';
+
+export class CreateUserDto {
+    
+  @IsString()
+  readonly firstName: string;
+
+  @IsString()
+  readonly lastName: string;
+
+  @IsString()
+  readonly email: string;
+
+  @IsString()
+  readonly age: string;
+}
---
+++ src/user/user.entity.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { PrimaryGeneratedColumn, BaseEntity, Column, Entity } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  age: string;
+}
